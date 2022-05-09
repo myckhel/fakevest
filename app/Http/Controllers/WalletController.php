47d23762fca9f6f2c3389c478e34f32c2418fd12
myckhel <myckhel1@hotmail.com>
2022-05-09 20:37:56 +0100

    implemented basic wallet

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Http\Request;
+
+class WalletController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $request->validate([
+      'orderBy'     => '',
+      'order'       => 'in:asc,desc',
+      'pageSize'    => 'int',
+    ]);
+
+    $pageSize = $request->pageSize;
+    $order    = $request->order;
+    $orderBy  = $request->orderBy;
+
+    return Wallet
+      ::orderBy($orderBy ?? 'id', $order ?? 'asc')
+      ->paginate($pageSize);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([]);
+    $user     = $request->user();
+
+    return Wallet::create($request->only([]));
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Wallet  $wallet
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Wallet $wallet)
+  {
+    $this->authorize('view', $wallet);
+    return $wallet;
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\Wallet  $wallet
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Wallet $wallet)
+  {
+    $this->authorize('update', $wallet);
+    $request->validate([]);
+    $user     = $request->user();
+
+    $wallet->update(array_filter($request->only($wallet->getFillable())));
+
+    return $wallet;
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Wallet  $wallet
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Wallet $wallet)
+  {
+    $this->authorize('delete', $wallet);
+
+    $wallet->delete();
+
+    return ['status' => true];
+  }
+}
---
+++ app/Models/Wallet.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Bavix\Wallet\Models\Wallet as BaseWallet;
+
+class Wallet extends BaseWallet
+{
+  use HasFactory;
+}
