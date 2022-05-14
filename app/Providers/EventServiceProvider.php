0ba2cf7b1adf7240f31dd5aa942b18d3028f0ed6
myckhel <myckhel1@hotmail.com>
2022-05-14 04:37:35 +0100

    created savings apis

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Saving;
+use App\Observers\SavingObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -9,34 +11,43 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+  /**
+   * The model observers for your application.
+   *
+   * @var array
+   */
+  protected $observers = [
+    Saving::class => [SavingObserver::class],
+  ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * The event listener mappings for the application.
+   *
+   * @var array<class-string, array<int, class-string>>
+   */
+  protected $listen = [
+    Registered::class => [
+      SendEmailVerificationNotification::class,
+    ],
+  ];
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     *
-     * @return bool
-     */
-    public function shouldDiscoverEvents()
-    {
-        return false;
-    }
+  /**
+   * Register any events for your application.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    //
+  }
+
+  /**
+   * Determine if events and listeners should be automatically discovered.
+   *
+   * @return bool
+   */
+  public function shouldDiscoverEvents()
+  {
+    return false;
+  }
 }
