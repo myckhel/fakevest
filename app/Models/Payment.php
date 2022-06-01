3d9fbdfef8bde634a3b6ce798eebc381dc56cc02
myckhel <myckhel1@hotmail.com>
2022-06-01 18:29:46 +0100

    bug fix

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,15 +14,17 @@ class Payment extends Model
   static function process(object $paymentDetails)
   {
     $payment          = static::firstOrCreate(
-      ['reference', $paymentDetails->reference],
+      ['reference' => $paymentDetails->reference],
       [
         'amount'        => $paymentDetails->amount,
-        'access_code'   => '',
+        'access_code'   => Carbon::now()->timestamp,
         'reference'     => $paymentDetails->reference,
+        'status'        => 'pending',
+        'user_id'       => User::whereEmail($paymentDetails->customer['email'])->first()->id,
       ]
-    )->first();
+    );
 
-    if ($payment && $payment->status == 'pending') {
+    if ($payment?->status == 'pending') {
       $user           = $payment->user;
       if ($paymentDetails->status != 'success') {
         $payment->update([
@@ -43,7 +46,7 @@ class Payment extends Model
 
         if ($paymentDetails->plan) {
           $saving = Saving::wherePaymentPlanId($paymentDetails->plan['id'])->first();
-          $wallet = $saving->wallet;
+          $wallet = $saving?->wallet;
           if ($wallet) {
             $wallet->deposit($wallet::fromKobo($paymentDetails->amount));
           } else {
