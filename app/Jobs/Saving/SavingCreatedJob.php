9459f6dd368f165a3077e6777493a2b8bd1c80ab
myckhel <myckhel1@hotmail.com>
2022-06-09 20:07:24 +0100

    handled one time savings

--- app/Jobs/Saving/SavingCreatedJob.php
+++ app/Jobs/Saving/SavingCreatedJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\Saving;
 
+use App\Models\PaymentOption;
 use App\Models\Saving;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
@@ -10,6 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Myckhel\Paystack\Support\Charge;
 use Myckhel\Paystack\Support\Plan;
 use Myckhel\Paystack\Support\Subscription;
 
@@ -36,19 +38,30 @@ class SavingCreatedJob implements ShouldQueue
     $saving   = $this->saving;
     $email    = $saving->user->email;
 
-    $plan = (object) Plan::create([
-      'name'        => $saving->desc,
-      'description' => $saving->desc,
-      'amount'      => $saving->amount * 10,
-      'interval'    => $saving->interval,
-    ])['data'];
+    if ($saving->interval) {
+      $plan = (object) Plan::create([
+        'name'        => $saving->desc,
+        'description' => $saving->desc,
+        'amount'      => $saving->amount * 10,
+        'interval'    => $saving->interval,
+      ])['data'];
 
-    Subscription::create([
-      'plan'   => $plan->plan_code,
-      'customer'    => $email,
-      'start_date'  => Carbon::now()->addSeconds(40)->toIso8601String(),
-    ]);
+      Subscription::create([
+        'plan'        => $plan->plan_code,
+        'customer'    => $email,
+        'start_date'  => Carbon::now()->addSeconds(40)->toIso8601String(),
+      ]);
 
-    $saving->update(['payment_plan_id' => $plan->id]);
+      $saving->update(['payment_plan_id' => $plan->id]);
+    } elseif ($saving->metas['payment_option_id']) {
+      $option = PaymentOption::find($saving->metas['payment_option_id']);
+
+      $option && Charge::create([
+        'authorization_code'  => $option->authorization_code,
+        'amount'              => $saving->amount,
+        'email'               => $saving->user->email,
+        'metadata'            => ['saving_id' => $saving->id]
+      ]);
+    }
   }
 }
