535e19cf58ac311c148c5f7dfa180a0a3344f3ec
Myckhel <myckhel1@hotmail.com>
2022-07-31 03:14:27 +0100

    created challenge won notice

--- lang/en/notice.php
+++ lang/en/notice.php
@@ -4,5 +4,8 @@ return [
   'savings' => [
     '_matured' => 'Your :name savings “:desc” has matured and has been withdrawn to your wallet',
     '_funded' => ':amount has been added to your saving plan “:desc”',
+  ],
+  'challenge' => [
+    '_won' => ':user. won your challenge “:desc”'
   ]
 ];
