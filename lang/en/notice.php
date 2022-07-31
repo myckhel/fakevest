c4cc5fe9c56364700d04019a1bdd644097d9fe8f
Myckhel <myckhel1@hotmail.com>
2022-07-31 01:47:15 +0100

    created savings funded notification

--- lang/en/notice.php
+++ lang/en/notice.php
@@ -3,5 +3,6 @@
 return [
   'savings' => [
     '_matured' => 'Your :name savings “:desc” has matured and has been withdrawn to your wallet',
+    '_funded' => ':amount has been added to your saving plan “:desc”',
   ]
 ];
