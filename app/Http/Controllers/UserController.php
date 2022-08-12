d3e746d0e0d0150784421e9f6cdffa5d1060ba7a
Myckhel <myckhel1@hotmail.com>
2022-08-12 23:29:40 +0100

    implemented portfolio

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
+  /**
+   * Get portfolio of the auth user.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  function portfolio(Request $request)
+  {
+    $request->validate([]);
+
+    $user = $request->user();
+
+    $transactionQuery = Transaction::belongsToUser($user);
+
+    $lifetime = $transactionQuery->whereType('deposit')->sum('amount');
+
+    $chart = $transactionQuery->latest()->limit(10)->get('amount')->pluck('amount');
+
+    $collect = Wallet::belongsToUser($user)
+      ->whereHas('trans', fn ($q) => $q->whereWithinDay('transactions'))
+      ->withSum(['trans as balance_change' => fn ($q) => $q->whereWithinDay('transactions')], 'amount')
+      ->withSum(
+        ['trans as balance_change_percentage' => fn ($q) => $q->whereWithinDay('transactions')],
+        DB::raw(Wallet::$changePercentageSyntax)
+      )->get();
+
+    $balance_change = $collect->sum('balance_change');
+    $balance_change_percentage = $collect->sum('balance_change_percentage');
+
+    $thisYear = $transactionQuery
+      ->whereRaw('YEAR(created_at) = YEAR(CURRENT_DATE())')
+      ->sum('amount');
+
+    $thisMonth = $transactionQuery
+      ->whereRaw('MONTH(created_at) = MONTH(CURRENT_DATE())')
+      ->whereRaw('YEAR(created_at) = YEAR(CURRENT_DATE())')
+      ->sum('amount');
+
+    $thisWeek = $transactionQuery
+      ->whereRaw('MONTH(created_at) = MONTH(CURRENT_DATE())')
+      ->whereRaw('YEAR(created_at) = YEAR(CURRENT_DATE())')
+      ->whereRaw('WEEK(created_at) = WEEK(CURRENT_DATE())')
+      ->sum('amount');
+
+    $walletLifetime = Wallet::whereHolderId($user->id)->whereHolderType($user::class)
+      ->whereType('deposit')
+      ->leftJoin('transactions', 'transactions.wallet_id', 'wallets.id')
+      ->sum('amount');
+
+    $savingPer = ($lifetime - $walletLifetime) / $lifetime * 100;
+
+    return [
+      'lifetime'  => (float) $lifetime,
+      'balance_change' => $balance_change,
+      'balance_change_percentage' => $balance_change_percentage,
+      'thisMonth' => (float) $thisMonth,
+      'thisYear'  => (float) $thisYear,
+      'thisWeek'  => (float) $thisWeek,
+      'net'       => [
+        'savings' => (float) $savingPer,
+        'wallet'  => 100 - $savingPer,
+      ],
+      'chart'     => $chart,
+    ];
+  }
   /**
    * Display a listing of the resource.
    *
--- routes/api.php
+++ routes/api.php
@@ -52,7 +52,7 @@ Route::group(['middleware' => ['auth:api']], function () {
   );
 
   Route::put('users/{user}/avatar',     [UserController::class, 'updateAvatar']);
-  Route::put('users/portfolio',         [UserController::class, 'portfolio']);
+  Route::get('users/portfolio',         [UserController::class, 'portfolio']);
 
   Route::post('wallets/withdraw',       [WalletController::class, 'withdraw']);
   Route::get('wallets/dollar',          [WalletController::class, 'viewDollar']);
